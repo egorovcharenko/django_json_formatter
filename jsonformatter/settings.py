621afaf00fc6bd7653f42f03b153683cac947307
Egor <egor.ovcharenko@gmail.com>
2018-02-25 22:04:05 +0300

    added requirements.txt

--- jsonformatter/settings.py
+++ jsonformatter/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'jsonformatter.apps.JsonformatterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
